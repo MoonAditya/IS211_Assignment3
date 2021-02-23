0121635d78343983140bf75d1745bd96a67b553b
Monmon Saha <saham2@upmc.edu>
2021-02-22 23:58:56 -0500

    updated

--- assignment3.py
+++ assignment3.py
@@ -1,15 +1,47 @@
+import urllib.request
+import csv
 import argparse
-# other imports go here
+import re
+import logging
 
+
+downloadUrl = 'http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv'
+def downloadData(url=downloadUrl):
+    g = urllib.request.urlopen(url)
+    name = g.url[url.rfind('/') + 1:]
+    with open(name, 'wb') as f:
+       return f.write(g.read())
+
+def processData(file='weblog.csv'):
+    with open(file) as csvFile:
+        reader = csv.reader(csvFile, delimiter= ",")
+        imgCount = 0
+        total_rows = 0
+        for eachRow in reader:
+            fileName = eachRow[0]
+            if(re.search("(.(?i)(jpe?g|png|gif|bmp))$", fileName,)):
+                imgCount+=1
+            
+            dateTime = eachRow[1]
+            browserInfo = eachRow[2]
+
+            status = eachRow[3]
+            fileSize = eachRow[4]
+            total_rows+=1 
+        print("Total image hit found".format(imgCount))
+        image_hit_percentage = round((imgCount / total_rows) * 100)
+        print("Image requests account for {}% of all requests".format(image_hit_percentage))
 
 def main(url):
-    print(f"Running main with URL = {url}...")
-
+     print(f"Running main with URL = {url}...")
+     parseUrl = downloadData()
+     processData(parseUrl)
 
 if __name__ == "__main__":
-    """Main entry point"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
-    args = parser.parse_args()
-    main(args.url)
-    
+     """Main entry point"""
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", help="URL to the datafile", type=str, required=False)
+args = parser.parse_args()
+logger = logging.getLogger("Assignment3 ")
+logging.basicConfig(filename='errors.log', level=logging.ERROR)
+main(args.url)
